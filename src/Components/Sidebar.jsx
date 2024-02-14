33fd2c5d0099cc7dacd9558f1f36d8a37bc2738b
dusengepeggy <dickinepeggy100@gmail.com>
2024-02-14 17:03:15 +0200

    api

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -66,7 +66,7 @@ const Sidebar = ({ children }) => {
 
   const logout = () => {
     localStorage.clear();
-    window.location.href = "http://localhost:5174/login";
+    window.location.href = "https://uhfe-ewqw9iobk-peggys-projects.vercel.app/login";
   };
 
   //button js handler
